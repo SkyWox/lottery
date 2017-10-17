a8d4cced5308df4df96209427de99b6898463cc7
Skylar Wilcox <lenin1916@gmail.com>
2017-10-16 19:26:06 -0700

    removed blank folder

---
+++ client/src/App.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import './App.css';
+import TicketContainer from './TicketContainer.js'
+
+class App extends Component {
+
+    componentWillMount(){
+      this.setState({
+        //default list to speed loading
+        nameDrop : ['powerball', 'superlplus', 'megamill'],
+        propDrop : ['Powerball', 'Super Lotto Plus', 'Mega Millions'],
+        tickets : [],
+      //can't use an async here or first render will be undefined
+      lottoname: 'powerball',
+      proper : 'Powerball'
+      })
+    }
+
+  componentDidMount() {
+    this.addTicket(false)
+    //put update of name and props here
+
+  }
+
+  async update(obj){
+    await this.setState(obj)
+    this.addTicket(false)
+  }
+
+  //arrow functions preserve this binding for calling w/in components
+  //https://daveceddia.com/avoid-bind-when-passing-props/
+    addTicket (mintBool) {
+    var ticketArray = this.state.tickets
+    ticketArray.push({
+      key : (new Date()).getTime(),
+      lottoname : this.state.lottoname,
+      mint : mintBool
+    })
+
+    this.setState({ tickets: ticketArray })
+    }
+
+    async getSpecs (name) {
+      var specs = {}
+      await fetch('/api/specs?name=' + name)
+      .then(res => res.json())
+      .then(function(res){ specs = res })
+      .catch()
+      return specs
+    }
+
+    remove(array, element) {
+      return array.filter(e => e !== element);
+    }
+
+  render() {
+    //remove the current one so it's just the other lottos
+    var currNameDrop = this.remove(this.state.nameDrop, this.state.lottoname)
+    var currPropDrop = this.remove(this.state.propDrop, this.state.proper)
+    return (
+      <div className="App">
+        <div>
+          <h1>
+              <div className="dropdown">
+                  <button className="dropbtn">{this.state.proper}</button>
+                    <div className="dropdown-content">
+                      {currNameDrop.map((name, index) =>
+                        <a key = {index} onClick={() => this.update({
+                            lottoname : currNameDrop[index],
+                            proper : currPropDrop[index],
+                            tickets : []})}
+                            value = {name}>
+                            {currPropDrop[index]}
+                        </a>
+                    )}
+                    </div>
+              </div>
+              Lucky Numbers</h1>
+            {this.state.tickets.map((timestamp, index) =>
+                <TicketContainer key = {index} num = {index}
+                  lottoname = {this.state.lottoname} mint = {timestamp.mint}/>
+            )}
+                <button
+                  className="addTicket"
+                  onClick={() => this.addTicket(false)}>
+                  + Add Ticket
+                </button>
+                <button
+                  className="addTicket"
+                  onClick={() => this.addTicket(true)}>
+                  + Add Fresh Ticket
+                </button>
+          </div>
+      </div>
+    );
+  }
+
+}
+
+export default App;
---
+++ client/src/TicketContainer.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import './App.css';
+import moment from 'moment'
+
+class TicketContainer extends Component {
+  // Initialize state
+  state = {
+    vanilla: [],
+    special : 0,
+    num : 1,
+    showWhat : false
+  }
+
+  componentWillMount(){
+    this.setState({ lottoname : this.props.lottoname, num : (this.props.num)})
+  }
+
+  componentWillReceiveProps (){
+    this.setState({ lottoname : this.props.lottoname, num : (this.props.num)})
+  }
+
+  // Fetch passwords after first mount
+  componentDidMount() {
+    this.getPowerballSpecial();
+  }
+
+  handleWhatisMint (){
+    if(this.state.showWhat){this.setState({showWhat:false})}else {
+      this.setState({showWhat:true})
+    }
+  }
+
+  getPowerballSpecial = () => {
+      const name = this.state.lottoname
+      var requestURL = new Request('/api/getnumbers?mint=' + this.props.mint + '&name=' + name + '&date=' + moment())
+      fetch(requestURL)
+      .then(res => res.json())
+      .then(res => this.setState({
+        special : res.special,
+        vanilla : res.vanilla
+       }))
+       .catch()
+    }
+
+    shuffle = () => {
+      this.getPowerballSpecial();
+    }
+
+  render() {
+    return (
+      <div className={"ticketContainer " + (this.props.mint ? "mint" : "")}>
+        <span className="ticketTitle">
+          Ticket {this.props.num + 1}
+        </span>
+            {this.props.mint === false &&
+              <button
+                className="shuffle"
+                onClick={this.shuffle}>
+                Shuffle
+              </button>}
+            {this.props.mint &&
+            <button
+              className = "addTicket"
+              style = {{float: 'left'}}
+              onClick={() => {this.handleWhatisMint()}}>
+              <span role='img' aria-label='mint icon'>🌿</span>
+                {this.state.showWhat ?
+                  'We guarantee nobody else has seen these fresh numbers!'
+                  : 'Minty Fresh Numbers!'
+                }
+            </button>
+            }
+              <br></br><br></br>
+            <div className="numberContainer">
+                {this.state.vanilla.map((luckynum, index) =>
+                  <li name={this.props.lottoname} key={index} value={luckynum} className="numberCircle vanillaNum">
+                    {luckynum}
+                  </li>
+                )}
+                {/*only show if there is a speical number*/}
+                {this.state.special !== 0 &&
+                  <li className="numberCircle specialNum">{this.state.special}</li>
+                }
+            </div>
+
+      </div>
+    );
+  }
+}
+
+export default TicketContainer;
