dccb2e21450b17de4034fd3f6d6a40d180870706
Skylar Wilcox <lenin1916@gmail.com>
2017-10-24 09:27:05 -0700

    deleted old code, correted cap on lottoList

--- routes/lottolist.js
+++ routes/lottolist.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const lottolist = require('../lottolist.json')
+const lottolist = require('../lottoList.json')
 
 router.get('/', (req, res) => {
   res.send(lottolist)
