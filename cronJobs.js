826982243b122469b0211ee72804cf035a70d7aa
Skylar Wilcox <lenin1916@gmail.com>
2017-12-05 11:14:47 -0800

    Last console log should be that scores have been updated

--- cronJobs.js
+++ cronJobs.js
@@ -17,24 +17,27 @@ module.exports = {
 			var potwon = true
 			contact.email('powerball', today, winningnums, potwon)
 		})*/
+		var updated = false
 
 		var fetchLottoResults = new CronJob(
-			'0 0/1 19-23 ? * 3,6',
+			'0 0/1 19-23 * * 3,6',
 			//drawing at 8pm PST
 			function() {
-				updateResults.mostRecent().then(currentDate => {
-					updateResults.scrapePBASP().then(answer => {
-						if (answer.dates.indexOf(currentDate) === -1) {
-							updateResults.updatePBDB(answer, true).then(() => {
-								console.log(
-									'updated scores at ' +
-										moment().format('MMM Do YYYY, h:mm:ss a')
-								)
-								updated = true
-							})
-						} else console.log('no updated score available')
+				if (updated === false) {
+					updateResults.mostRecent().then(currentDate => {
+						updateResults.scrapePBASP().then(answer => {
+							if (answer.dates.indexOf(currentDate) === -1) {
+								updateResults.updatePBDB(answer, true).then(() => {
+									console.log(
+										'updated scores at ' +
+											moment().format('MMM Do YYYY, h:mm:ss a')
+									)
+									updated = true
+								})
+							} else console.log('no updated score available')
+						})
 					})
-				})
+				}
 			},
 			true, //start job now?
 			'America/Los_Angeles' //timezone
