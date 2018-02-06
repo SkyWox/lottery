76ba5f612eaac8a537060f2680f40962caa12a39
Skylar Wilcox <lenin1916@gmail.com>
2018-02-06 12:03:35 -0800

    Rewrote panel component to be compatible with react-bootstrap update

--- client/src/components/TicketContainer.js
+++ client/src/components/TicketContainer.js
@@ -9,35 +9,34 @@ class TicketContainer extends Component {
       <div style={{ align: 'center' }}>
         <Col xs={12} sm={7} md={5}>
           <Panel
-            className="bootstrap-overrides"
-            header={
-              <div>
-                <LottoLogo lottoname={this.props.ticket.lottoname} />
-                {this.props.ticket.lottodate}
-              </div>
-            }
             bsStyle={
               this.props.ticket.lottoname === 'powerball' ? 'info' : 'success'
             }
           >
-            <div className="numberContainer">
-              {this.props.ticket.vanillanums.map((number, index) => (
-                <li
-                  name={this.props.ticket.lottoname}
-                  key={index}
-                  value={number}
-                  className="numberCircle vanillaNum"
-                >
-                  {number}
-                </li>
-              ))}
-              {/*only show if there is a special number*/}
-              {this.props.ticket.specialnums !== 0 && (
-                <li className="numberCircle specialNum">
-                  {this.props.ticket.specialnums}
-                </li>
-              )}
-            </div>
+            <Panel.Heading>
+              <LottoLogo lottoname={this.props.ticket.lottoname} />
+              {this.props.ticket.lottodate}
+            </Panel.Heading>
+            <Panel.Body>
+              <div className="numberContainer">
+                {this.props.ticket.vanillanums.map((number, index) => (
+                  <li
+                    name={this.props.ticket.lottoname}
+                    key={index}
+                    value={number}
+                    className="numberCircle vanillaNum"
+                  >
+                    {number}
+                  </li>
+                ))}
+                {/*only show if there is a special number*/}
+                {this.props.ticket.specialnums !== 0 && (
+                  <li className="numberCircle specialNum">
+                    {this.props.ticket.specialnums}
+                  </li>
+                )}
+              </div>
+            </Panel.Body>
           </Panel>
         </Col>
       </div>
